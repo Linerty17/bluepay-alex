2a9985ba9e8fd5ae86c28edc85a43c5b00f62bbd
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-12-09 16:40:28 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -115,11 +115,11 @@ const BuyBPCPayment = () => {
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-2xl font-bold">9128941506</p>
+            <p className="text-2xl font-bold">8130524083</p>
             <Button
               variant="default"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2"
-              onClick={() => handleCopy("9128941506", "Account Number")}
+              onClick={() => handleCopy("8130524083", "Account Number")}
             >
               <Copy size={18} />
               Copy
@@ -129,7 +129,7 @@ const BuyBPCPayment = () => {
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Bank Name</p>
-          <p className="text-2xl font-bold">PALMPAY BANK</p>
+          <p className="text-2xl font-bold">MOMO PSB</p>
         </div>
 
         <div className="mb-4 border-t pt-4">
