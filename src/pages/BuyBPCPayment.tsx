fc4f3ed3e289043e212366a8191031830392aca4
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-12-30 10:09:04 +0100

    Update account details in BuyBPCPayment component

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -115,11 +115,11 @@ const BuyBPCPayment = () => {
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-2xl font-bold">8024419509</p>
+            <p className="text-2xl font-bold">5070811364</p>
             <Button
               variant="default"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2"
-              onClick={() => handleCopy("8024419509", "Account Number")}
+              onClick={() => handleCopy("5070811364", "Account Number")}
             >
               <Copy size={18} />
               Copy
@@ -129,12 +129,12 @@ const BuyBPCPayment = () => {
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Bank Name</p>
-          <p className="text-2xl font-bold">Zenith easy wallet</p>
+          <p className="text-2xl font-bold">Moniepoint MBF</p>
         </div>
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Name</p>
-          <p className="text-2xl font-bold">TOLUWALSA ALAILEDOYE</p>
+          <p className="text-2xl font-bold">Abraham Olanegan</p>
         </div>
       </div>
 
